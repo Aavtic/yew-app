9467cfb567b9029dc77b3d367d7a07ac5a77899e
Aavtic <aadishm79@gmail.com>
2024-09-21 22:14:47 +0530

    Say Hi update '

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,59 @@
 use yew::prelude::*;
 
 enum Msg {
-    AddOne,
-    SubOne,
+    SayHi(String),
+    Submit,
 }
 
-
-struct CounterComponent {
-    count: i64,
+struct Text {
+    msg: String,
+    greeting_msg: String,
 }
 
-impl Component for CounterComponent {
+impl Component for Text {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { count : 0 }
+        Self {
+            msg: String::new(),
+            greeting_msg: String::new(),
+        }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Msg) -> bool{
+    fn update(&mut self, _ctx: &Context<Self>, msg: Msg) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.count += 1;
+            Msg::SayHi(msg) => {
+                self.msg = msg;
                 true
             },
-            Msg::SubOne => {
-                self.count -= 1;
+            Msg::Submit => {
+                self.greeting_msg = format!("Hi {}", self.msg);
                 true
             }
-        }
-    } 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let link = ctx.link();
-        html! {
-            <div class="container">
-                <p> {self.count} </p>
-                <button onclick={link.callback(|_| Msg::AddOne)}> {"+1"} </button>
-                <button onclick={link.callback(|_| Msg::SubOne)}> {"-1"} </button>
-            </div>
+
         }
     }
 
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <input
+                    type="text"
+                    oninput={ctx.link().callback(|e: InputEvent| {
+                        let input = e.target_unchecked_into::<web_sys::HtmlInputElement>();
+                        Msg::SayHi(input.value())
+                    })}
+                    placeholder="Enter your name"
+                />
+                <button onclick={ctx.link().callback(|_| Msg::Submit)}>{"Submit"}</button>
+                <div>{ &self.greeting_msg }</div>
+            </div>
+        }
+    } 
 }
 
-
 fn main() {
-    yew::Renderer::<CounterComponent>::new().render();
+    yew::Renderer::<Text>::new().render();
 }
 
